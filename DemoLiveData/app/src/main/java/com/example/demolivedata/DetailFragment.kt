ec85cc107905354ba8625bc116ca74b80a4b1da5
huatrung <huatrunghieu181@gmail.com>
2021-04-19 23:32:00 +0700

    create
    create project
    
    Co-Authored-By: Hieu K. Le <hieulk@gmail.com>

---
+++ DemoLiveData/app/src/main/java/com/example/demolivedata/DetailFragment.kt
@@ -0,0 +1,83 @@
+package com.example.demolivedata
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    lateinit var medalViewModel: MedalViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        medalViewModel = ViewModelProviders.of(activity!!).get(MedalViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        medalViewModel.numberOfGoldMedal.observe(activity!!, Observer {
+            displayMedal()
+        })
+        medalViewModel.numberOfSilverMedal.observe(activity!!, Observer {
+            displayMedal()
+        })
+        medalViewModel.numberOfBronzeMedal.observe(activity!!, Observer {
+            displayMedal()
+        })
+    }
+
+    private fun displayMedal() {
+        tvDetailGoldNumber.text= activity?.getString(R.string.number_of_gold_label,medalViewModel?.numberOfGoldMedal.value)
+        tvDetailSilverNumber.text=activity?.getString(R.string.number_of_silver_label, medalViewModel?.numberOfSilverMedal.value)
+        tvDetailBronzeNumber.text=activity?.getString(R.string.number_of_bronze_label, medalViewModel?.numberOfBronzeMedal.value)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment DetailFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
